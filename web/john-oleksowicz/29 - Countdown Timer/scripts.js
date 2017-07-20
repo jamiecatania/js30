ba79a09f17529c8519e42ec41e4ffeb6c7c25d34
John Oleksowicz <john.oleksowicz@govdelivery.com>
2017-07-20 09:52:31 -0500

    FInalizing lesson 29

--- web/john-oleksowicz/29 - Countdown Timer/scripts.js
+++ web/john-oleksowicz/29 - Countdown Timer/scripts.js
@@ -1,18 +1,23 @@
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+const minutesForm = document.querySelector('#custom');
+let countdownInterval;
 
 function timer(seconds) {
+  clearInterval(countdownInterval);
+
   const now = Date.now();
   const then = now + seconds * 1000;
 
   displayTimeLeft(seconds);
   displayEndTime(then);
 
-  const countdown = setInterval(() => {
+  countdownInterval = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
 
     if (secondsLeft < 0) {
-      clearInterval(countdown);
+      clearInterval(countdownInterval);
       return;
     }
     displayTimeLeft(secondsLeft);
@@ -37,3 +42,15 @@ function displayEndTime(timestamp) {
 function numberWithLeadingZero(number) {
   return number < 10 ? '0' + number : number;
 }
+
+buttons.forEach((button) => {
+  button.addEventListener('click', function() {
+    timer(this.dataset.time);
+  });
+});
+
+minutesForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  timer(this.querySelector('input').value * 60);
+  this.reset();
+});
